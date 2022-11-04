9d4c0b118de7f53a4abb96d8d8eb78c3ccf56f4c
egiho <dorkassinambela85@gmail.com>
2022-11-04 18:34:27 +0700

    buat list article

--- aplikasi-backend/app/Http/Controllers/Me/ArticleController.php
+++ aplikasi-backend/app/Http/Controllers/Me/ArticleController.php
@@ -5,14 +5,43 @@ namespace App\Http\Controllers\Me;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\Me\Article\StoreRequest;
+use App\Models\Article;
 use Str;
 use ImageKit\ImageKit;
 use App\Models\User;
+use App\Models\Article;
 
 class ArticleController extends Controller
 {
+    public function index()
+    {
+        // get user id yang saat ini sedang login
+        // get article dimana user id nya yang sat ini sedang login
+        // get juga category dan usernya siapa
+        // buat paginationnya
+
+        $userId = auth()->id();
+
+        $articles = Article::with(['category', 'user:id,name,email,picture,'])->select([
+            'id', 'user_id', 'category_id', 'title', 'slug', 'content_preview', 'featured_image', 'created_at', 'updated_at',
+        ])
+
+            ->where('user_id', $userId)
+            ->paginate();
+
+        return response()->json([
+            'meta' => [
+                'code' => 200,
+                'status' => 'success',
+                'message' => 'Articles Fetched Successfully.',
+            ],
+            'data' => $articles,
+        ]);
+
+    }
     public function store(StoreRequest $Request)
     {
+
         // category_id
         // title
         // content

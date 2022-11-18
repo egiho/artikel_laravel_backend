d2c75cfe2fd3afc85fe1cf8b31f113cd388043fb
egiho <dorkassinambela85@gmail.com>
2022-11-18 22:32:17 +0700

    buat update me article

--- aplikasi-backend/app/Http/Controllers/Me/ArticleController.php
+++ aplikasi-backend/app/Http/Controllers/Me/ArticleController.php
@@ -5,12 +5,13 @@ namespace App\Http\Controllers\Me;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\Me\Article\StoreRequest;
+use App\Http\Requests\Me\Article\UpdateRequest;
 use App\Models\Article;
 use Str;
 use ImageKit\ImageKit;
 use App\Models\User;
 use App\Models\Article;
-use Error;
+
 
 class ArticleController extends Controller
 {
@@ -150,5 +151,170 @@ class ArticleController extends Controller
             ],
             'data' => [],
         ], 404);
+    }
+
+    public function update(UpdateRequest $request, $id)
+    {
+        // category_id
+        // title
+        // content
+        // featured_image
+
+        // get article berdasarkan id
+        // cek apakah get article berhasil
+        // jika berhasil get article maka get user id yg sedang login
+        // cek apakah user_id article sama dengan id user yg login
+        // jika iya maka get semua request yg valid
+        // generate slug dari title
+        // generate content_preview berdasarkan content
+        // cek apakah ada request featured_image
+        // jika iya maka upload gambar ke imagekit
+        // get url hasil upload dari imagekit
+        // lakukan update article dengan request valid dan data yg kita generate auto di controller ini
+        // cek apakah update article berhasil
+        // jika iya maka kembalikan response success
+        // (jika line ini dieksekusi artinya tidak berhasil update article) kembalikan response error 500, gagal update article
+        // (jika line ini dieksekusi artinya article ini bukan milik user yg login) kembalikan response 401, unauthorized
+        // (jika line ini dieksekusi artinya tidak berhasil get article) kembalikan response 404, article tidak ditemukan
+
+        $article = Article::find($id);
+
+        if ($article)
+        {
+            $userId = auth()->id();
+
+            if ($article->user_id === $userId)
+            {
+                $validated = $request->validated();
+
+                $validated['slug'] = Str::of($validated['title'])->slug('-') . '-' . time();
+                $validated['content_preview'] = substr($validated['content'], 0, 218) . '...';
+
+                if ($request->hasFile('featured_image'))
+                {
+                    $imageKit = new ImageKit(
+                        env('IMAGEKIT_PUBLIC_KEY'),
+                        env('IMAGEKIT_PRIVATE_KEY'),
+                        env('IMAGEKIT_URL_ENDPOINT'),
+                    );
+
+                    $image = base64_encode(file_get_contents($request->file('featured_image')));
+
+                    $uploadImage = $imageKit->uploadFile([
+                        'file' => $image,
+                        'fileName' => $validated['slug'],
+                        'folder' => '/article',
+                    ]);
+
+                    $validated['featured_image'] = $uploadImage->result->url;
+                }
+
+                $updateArticle = $article->update($validated);
+
+                if ($updateArticle)
+                {
+                    return response()->json([
+                        'meta' => [
+                            'code' => 200,
+                            'status' => 'success',
+                            'message' => 'Article updated successfully.',
+                        ],
+                        'data' => [],
+                    ]);
+                }
+
+                return response()->json([
+                    'meta' => [
+                        'code' => 500,
+                        'status' => 'error',
+                        'message' => 'Error! Article failed to update.',
+                    ],
+                    'data' => [],
+                ], 500);
+            }
+
+            return response()->json([
+                'meta' => [
+                    'code' => 401,
+                    'status' => 'error',
+                    'message' => 'Unauthorized.',
+                ],
+                'data' => [],
+            ], 401);
+        }
+
+        return response()->json([
+            'meta' => [
+                'code' => 404,
+                'status' => 'error',
+                'message' => 'Article not found.',
+            ],
+            'data' => [],
+        ], 404);
+    }
+
+    public function destroy($id)
+    {
+        // get article by id
+        // cek apakah article ada
+        // jika iya maka get user id yg sedang login
+        // cek apakah user id article sama dengan id dari user yg sedang login
+        // jika iya maka delete article
+        // cek apakah article berhasil didelete
+        // jika iya maka kembalikan response success
+        // (jika kode ini dieksekusi maka artinya article gagal didelete) kembalikan response error 500
+        // (jika kode ini dieksekusi maka artinya article ini bukan milik user yg login) kembalikan response error 401, unauthorized
+        // (jika kode ini dieksekusi maka artinya article tidak ditemukan) kembalikan response error 404, article not found
+
+        $article = Article::find($id);
+
+        if ($article)
+        {
+            $userId = auth()->id();
+
+            if ($article->user_id === $userId)
+            {
+                $deleteArticle = $article->delete();
+
+                if ($deleteArticle)
+                {
+                    return response()->json([
+                        'meta' => [
+                            'code' => 200,
+                            'status' => 'success',
+                            'message' => 'Article deleted successfully.',
+                        ],
+                        'data' => [],
+                    ]);
+                }
+
+                return response()->json([
+                    'meta' => [
+                        'code' => 500,
+                        'status' => 'error',
+                        'message' => 'Error! Article failed to delete.',
+                    ],
+                    'data' => [],
+                ], 500);
+            }
+
+            return response()->json([
+                'meta' => [
+                    'code' => 401,
+                    'status' => 'error',
+                    'message' => 'Unauthorized.',
+                ],
+                'data' => [],
+            ], 401);
+        }
+
+        return response()->json([
+            'meta' => [
+                'code' => 404,
+                'status' => 'error',
+                'message' => 'Article not found.',
+            ],
+            'data' => [],
+        ]);
     }
 }
---
+++ aplikasi-backend/app/Http/Requests/Me/Article/UpdateRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests\Me\Article;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Traits\ErrorResponseJson;
+use Illuminate\Validation\Rule;
+use App\Models\Category;
+
+class UpdateRequest extends FormRequest
+{
+    use ErrorResponseJson;
+
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'category_id' => 'required|' . Rule::in(Category::pluck('id')),
+            'title' => 'required|string|max:190',
+            'content' => 'required|string',
+            'featured_image' => 'nullable|image|mimes:jpg,jpeg,bmp,png',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'category_id' => 'category'
+        ];
+    }
+}
